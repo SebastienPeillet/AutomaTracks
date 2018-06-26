a7ed1efd336251b06b8c782b393ce4ca1782683c
Peillet Sebastien <peillet.seb@gmail.com>
2018-06-26 10:55:22 -0300

    change docks to support french translation

--- i18n/AutomaTracks_fr.ts
+++ i18n/AutomaTracks_fr.ts
@@ -4,111 +4,111 @@
 <context>
     <name>AutomaTracksDockWidgetBase</name>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="14"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="20"/>
         <source>AutomaTracks</source>
         <translation>AutomaTracks</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="37"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="43"/>
         <source>Main_Onglet</source>
         <translation>Menu principal</translation>
     </message>
     <message utf8="true">
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="59"/>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="98"/>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="329"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="65"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="104"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="341"/>
         <source>↺</source>
         <translation>↺</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="72"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="78"/>
         <source>Path point layer :</source>
         <translation>Couche de points :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="85"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="91"/>
         <source>DEM layer :</source>
         <translation>Couche MNT :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="121"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="127"/>
         <source>Edges number :</source>
         <translation>Nombre de liaisons possibles pour un noeud :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="134"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="140"/>
         <source>Direction constraint method :</source>
         <translation>Méthode de contrainte de direction :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="147"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="153"/>
         <source>Maximum length slope (%) :</source>
         <translation>Pente maximum (%) :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="173"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="179"/>
         <source>Launch</source>
         <translation>Démarrer</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="189"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="195"/>
         <source>8</source>
         <translation>8</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="199"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="205"/>
         <source>24</source>
         <translation>24</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="209"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="215"/>
         <source>40</source>
         <translation>40</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="219"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="225"/>
         <source>48</source>
         <translation>48</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="241"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="247"/>
         <source>degree</source>
-        <translation>degré d'angle</translation>
+        <translation>degré d&apos;angle</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="251"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="263"/>
         <source>radius of curvature</source>
         <translation>rayon de courbure</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="270"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="282"/>
         <source>Output path :</source>
-        <translation>Chemin du fichier en sortie</translation>
+        <translation>Chemin du fichier en sortie :</translation>
     </message>
     <message utf8="true">
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="293"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="305"/>
         <source>Direction threshold (° or m) :</source>
         <translation>Seuil de contrainte de direction (° or m) :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="342"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="354"/>
         <source>Mask layer :</source>
-        <translation>Couche de masque (valeur 0)</translation>
+        <translation>Couche de masque :</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="348"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="360"/>
         <source>Pre_tools</source>
         <translation>Pré-traitement</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="360"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="372"/>
         <source>Convert ridges to point network</source>
-        <translation>Convertir polylignes en réseau de point</translation>
+        <translation>Convertir des polylignes en réseau de point</translation>
     </message>
     <message>
-        <location filename="../AutomaTracks_dockwidget_base.ui" line="373"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="385"/>
         <source>Re-order point network from a begin point</source>
-        <translation>Ordonner le réseau de point par rapport à un exutoire</translation>
+        <translation>Ordonner le réseau depuis un exutoire</translation>
     </message>
 </context>
 <context>
@@ -164,7 +164,7 @@
     <message>
         <location filename="../reOrderDock.ui" line="59"/>
         <source>Outlet layer :</source>
-        <translation>Couche d'exutoire :</translation>
+        <translation>Couche d&apos;exutoire :</translation>
     </message>
     <message>
         <location filename="../reOrderDock.ui" line="82"/>
