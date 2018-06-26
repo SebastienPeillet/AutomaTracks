14f120a5290bfcb5facc98821cd1e43e51126cc7
Peillet Sebastien <peillet.seb@gmail.com>
2018-06-26 10:29:20 -0300

    add french translation

---
+++ i18n/AutomaTracks.pro
@@ -0,0 +1,11 @@
+FORMS = ../AutomaTracks_dockwidget_base.ui \
+../reOrderDock.ui \
+../ridgeToPointDock.ui
+
+SOURCES = ../AutomaTracks.py \
+../AutomaTracks_dockwidget.py \
+../reOrder_Dock.py \
+../ridgeToPoint_Dock.py
+
+TRANSLATIONS = AutomaTracks_fr.ts \
+
---
+++ i18n/AutomaTracks_fr.ts
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>AutomaTracksDockWidgetBase</name>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="14"/>
+        <source>AutomaTracks</source>
+        <translation>AutomaTracks</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="37"/>
+        <source>Main_Onglet</source>
+        <translation>Menu principal</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="59"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="98"/>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="329"/>
+        <source>↺</source>
+        <translation>↺</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="72"/>
+        <source>Path point layer :</source>
+        <translation>Couche de points :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="85"/>
+        <source>DEM layer :</source>
+        <translation>Couche MNT :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="121"/>
+        <source>Edges number :</source>
+        <translation>Nombre de liaisons possibles pour un noeud :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="134"/>
+        <source>Direction constraint method :</source>
+        <translation>Méthode de contrainte de direction :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="147"/>
+        <source>Maximum length slope (%) :</source>
+        <translation>Pente maximum (%) :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="173"/>
+        <source>Launch</source>
+        <translation>Démarrer</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="189"/>
+        <source>8</source>
+        <translation>8</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="199"/>
+        <source>24</source>
+        <translation>24</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="209"/>
+        <source>40</source>
+        <translation>40</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="219"/>
+        <source>48</source>
+        <translation>48</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="241"/>
+        <source>degree</source>
+        <translation>degré d'angle</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="251"/>
+        <source>radius of curvature</source>
+        <translation>rayon de courbure</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="270"/>
+        <source>Output path :</source>
+        <translation>Chemin du fichier en sortie</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="293"/>
+        <source>Direction threshold (° or m) :</source>
+        <translation>Seuil de contrainte de direction (° or m) :</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="342"/>
+        <source>Mask layer :</source>
+        <translation>Couche de masque (valeur 0)</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="348"/>
+        <source>Pre_tools</source>
+        <translation>Pré-traitement</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="360"/>
+        <source>Convert ridges to point network</source>
+        <translation>Convertir polylignes en réseau de point</translation>
+    </message>
+    <message>
+        <location filename="../AutomaTracks_dockwidget_base.ui" line="373"/>
+        <source>Re-order point network from a begin point</source>
+        <translation>Ordonner le réseau de point par rapport à un exutoire</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="14"/>
+        <source>Ridge to point network</source>
+        <translation>Réseau de point à partir de polylignes</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="26"/>
+        <source>Lines layer :</source>
+        <translation>Couche de polylignes :</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="39"/>
+        <source>Output layer :</source>
+        <translation>Couche en sortie :</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="85"/>
+        <source>Id line :</source>
+        <translation>Identifiant de ligne :</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="98"/>
+        <source>Min length between point :</source>
+        <translation>Distance minimum entre les points :</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="111"/>
+        <source>DEM layer :</source>
+        <translation>Couche MNT :</translation>
+    </message>
+    <message>
+        <location filename="../ridgeToPointDock.ui" line="134"/>
+        <source>Launch</source>
+        <translation>Démarrer</translation>
+    </message>
+</context>
+<context>
+    <name>reOrderDock</name>
+    <message>
+        <location filename="../reOrderDock.ui" line="14"/>
+        <source>reOrderDock</source>
+        <translation>Ordonner un réseau</translation>
+    </message>
+    <message>
+        <location filename="../reOrderDock.ui" line="46"/>
+        <source>Point network layer :</source>
+        <translation>Couche de réseau de point :</translation>
+    </message>
+    <message>
+        <location filename="../reOrderDock.ui" line="59"/>
+        <source>Outlet layer :</source>
+        <translation>Couche d'exutoire :</translation>
+    </message>
+    <message>
+        <location filename="../reOrderDock.ui" line="82"/>
+        <source>Output layer :</source>
+        <translation>Couche en sortie :</translation>
+    </message>
+    <message>
+        <location filename="../reOrderDock.ui" line="95"/>
+        <source>Launch</source>
+        <translation>Démarrer</translation>
+    </message>
+</context>
+</TS>
